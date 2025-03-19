fcd9e119af980873794c1927791c17c6bf5236d3
bassmaelabassi <elabassibasma31@gmail.com>
2025-03-19 15:01:07 +0100

    ajout la page dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Dashboard />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
